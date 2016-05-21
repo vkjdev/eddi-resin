f225ba306c86dca5c0b9f770085c9fc5cc9348d8
Tony <terimyaki@users.noreply.github.com>
2016-05-21 17:29:00 -0400

    added entry shell to clone repos

---
+++ entry.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+GITHUB_ROOT=https://eddibot:6d159d67d45981282216bc85be75ed5e7876a73f@github.com/water-alchemists
+
+git clone $GITHUB_ROOT/eddi-controls.git
+git clone $GITHUB_ROOT/eddi-persist.git
+
+(cd eddi-persist && npm install)
+(cd eddi-controls && npm install)
