fa262145f968d1cdc6d25c7d965c01af857d852d
Tony <terimyaki@users.noreply.github.com>
2016-06-04 18:41:45 -0400

    added git checking

--- entry.sh
+++ entry.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
 GITHUB_ROOT=https://eddibot:6d159d67d45981282216bc85be75ed5e7876a73f@github.com/water-alchemists
+PERSIST=eddi-persist
+CONTROLS=eddi-controls
 
-# git clone $GITHUB_ROOT/eddi-controls.git
-# git clone $GITHUB_ROOT/eddi-persist.git
+if [ -d "$PERSIST" ]; then
+    (cd $PERSIST && git pull origin master && npm install)
+else
+    git clone $GITHUB_ROOT/$PERSIST.git
+    (cd $PERSIST && npm install)
+fi
 
-# (cd eddi-persist && npm install)
-# (cd eddi-controls && npm install)
+if [ -d "$CONTROLS" ]; then
+    (cd $CONTROLS && git pull origin master && npm install)
+else
+    git clone $GITHUB_ROOT/$CONTROLS.git
+    (cd $CONTROLS && npm install)
+fi
 
 npm start
