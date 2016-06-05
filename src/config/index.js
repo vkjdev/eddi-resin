597e779f57342d5df33c88ed53b630d5830fd96f
Tony <terimyaki@users.noreply.github.com>
2016-06-05 16:03:05 -0400

    removed unused stuff

--- src/config/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-const isProduction = process.env.NODE_ENV === 'production',
-    prodConfig = require('./production'),
-    devConfig = require('./development'),
-    config = Object.assign({}, { production : isProduction }, isProduction ? prodConfig : devConfig );
-    
-module.exports = config;
